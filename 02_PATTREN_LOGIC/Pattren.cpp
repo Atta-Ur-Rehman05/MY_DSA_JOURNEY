33273be6212058deb0eaf859b1db21ef55149911
Atta ur rehman <attaur5565@gmail.com>
2026-04-04 11:57:11 +0500

    new problem practiced

---
+++ 02_PATTREN_LOGIC/Pattren.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+
+// print ractangle of *
+void printRactangle(int n){
+
+for(int i = 0 ; i < n; i++){
+    for(int j = 0 ; j < n; j++){
+        cout<< "*";
+    }
+    cout<< endl;
+}
+}
+
+
+
+
+
+
+
+
+
+int main(){
+ printRactangle(5);
+
+
+
+
+
+
+
+    return 0;
+}
