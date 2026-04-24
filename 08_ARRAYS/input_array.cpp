1b765773c0d176e9d7736ddb9f6803c023d25651
Atta ur rehman <attaur5565@gmail.com>
2026-04-24 14:15:59 +0500

    array updated

---
+++ 08_ARRAYS/input_array.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // taking  array as input
+
+    int array[5];
+    cout<< "input array element of size 5";
+    for(int i = 0; i < 5 ; i++){
+        cin>> array[i];
+    }
+
+    // display the input array
+    for(int i = 0; i < 5 ; i++){
+        cout<< array[i];
+    }
+
+
+
+
+
+
+
+    
+    return 0;
+}
